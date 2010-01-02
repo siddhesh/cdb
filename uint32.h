d3da682cb718efb3d21447c24e509d0ec10c7ff2
Siddhesh Poyarekar <siddhesh.poyarekar@gmail.com>
2010-01-02 23:49:04 +0530

    Autoolized code

--- uint32.h
+++ uint32.h
@@ -1,7 +1,11 @@
 #ifndef UINT32_H
 #define UINT32_H
 
-typedef unsigned int uint32;
+#if SIZEOF_LONG == 4
+	typedef unsigned long uint32;
+#else
+	typedef unsigned int uint32;
+#endif
 
 extern void uint32_pack(char *,uint32);
 extern void uint32_pack_big(char *,uint32);
